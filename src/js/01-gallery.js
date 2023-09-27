537ab6c194ca98b7ebb7d3a6af5c92554bdfd4ba
AlexKat <katsubo79@gmail.com>
2023-09-27 14:45:05 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImage = document.querySelector('.gallery');
+const imageMarkup = createImageCardsMarkup(galleryItems);
+
+galleryImage.insertAdjacentHTML('beforeend', imageMarkup);
+
+galleryImage.addEventListener('click', onGalleryImageClick);
+
+function createImageCardsMarkup(galleryItems) {
+return galleryItems
+.map(({ preview, original, description }) =>
+`
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+).join('');
+ }
+
+function onGalleryImageClick(evt) {
+    console.log(evt);
+    // if (evt.target === evt.carrentTarget) {
+    //     return
+    // }
+ }
